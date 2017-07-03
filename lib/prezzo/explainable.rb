5983f605fae58f82f8f37d048954c20e845bbfe5
Juan Ibiapina <juanibiapina@gmail.com>
2017-07-03 18:17:09 +0200

    feature: Add rescursive explanations

--- lib/prezzo/explainable.rb
+++ lib/prezzo/explainable.rb
@@ -7,14 +7,20 @@ module Prezzo
     end
 
     module ClassMethods
-      def explain_with(*methods)
+      def explain_with(*component_names)
         define_method(:explain) do
           explanation = {
             total: calculate,
           }
 
-          components = methods.each_with_object({}) do |method, acc|
-            acc[method] = send(method)
+          components = component_names.each_with_object({}) do |component, acc|
+            value = send(component)
+            if self.class.respond_to?(:components) && self.class.components.include?(component)
+              value = cached_components[component]
+            end
+            value = value.explain if value.respond_to?(:explain)
+            value = value.calculate if value.respond_to?(:calculate)
+            acc[component] = value
           end
 
           explanation[:components] = components unless components.empty?
